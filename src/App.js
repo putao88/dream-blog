f2b63c6052cad2a711e273628f6dbc93e3d95a59
litao <c-litao1@genomics.cn>
2020-05-08 11:06:04 +0800

    nothing

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588832748652&di=a0fff51d7fee1cfa10b515c8755d48a1&imgtype=0&src=http%3A%2F%2Fimg.mp.sohu.com%2Fupload%2F20170816%2Ffe1419ef7dad495e98ca77b14db43252_th.png" alt=""/>
+        <img style={{width:'400px', height:'300px'}}src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588832748652&di=a0fff51d7fee1cfa10b515c8755d48a1&imgtype=0&src=http%3A%2F%2Fimg.mp.sohu.com%2Fupload%2F20170816%2Ffe1419ef7dad495e98ca77b14db43252_th.png" alt=""/>
       </header>
     </div>
   );
